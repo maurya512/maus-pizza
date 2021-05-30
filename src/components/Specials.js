48e7273d445f644b45a2a5e6ad3173ef4366b748
maurya512 <patelmaurya0512@gmail.com>
2021-05-30 15:27:39 -0400

    added SpecialData and SpecialCard into the component

--- src/components/Specials.js
+++ src/components/Specials.js
@@ -1,10 +1,38 @@
+// import dependencies
 import React from 'react'
 
+// import the files needed
+import SpecialCard from './SpecialCard'
+import SpecialData from './SpecialData'
+
+// functional component
 function Specials() {
-    return (
-        <h1>
-          This is the specials page  
-        </h1>    )
+  return (
+    <>
+      <div className="my-5">
+        <h1 className="text-center"><strong>Specials</strong></h1>
+      </div>
+      <div className="container-fluid mb-5">
+        <div className="row">
+          <div className="col-10 mx-auto">
+            <div className="row gy-4">
+              {/* mapping through the array of objects in the special data */}
+              {
+                SpecialData.map((val, ind) => {
+                  return <SpecialCard key={ind}
+                    imgsrc={val.imgsrc}
+                    title={val.title}
+                    description={val.description}
+                    price={val.price}
+                  />
+                })
+              }
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Specials
