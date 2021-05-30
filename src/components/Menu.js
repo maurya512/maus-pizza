3837c124e45fbc44d980f32d0b0868a7e94c0e6d
maurya512 <patelmaurya0512@gmail.com>
2021-05-29 23:30:56 -0400

    imported the card and data components

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,10 +1,38 @@
+// import dependencies
 import React from 'react'
 
+// import files
+import Card from './Card'
+import Data from './Data'
+
+// functional Menu component
 function Menu() {
     return (
-        <h1>
-            This is the menu page   
-        </h1>
+        <>
+            <div className="my-5">
+                <h1 className="text-center">Pizza</h1>
+            </div>
+            <div className="container-fluid mb-5">
+                <div className="row">
+                    <div className="col-10 mx-auto">
+                        <div className="row gy-4">
+                            {/* mapping through the array of objects in the data component */}
+                            {
+                                Data.map((val, ind) => {
+                                    return <Card key={ind}
+                                        imgsrc={val.imgsrc}
+                                        title={val.title}
+                                        description={val.description}
+                                        price={val.price}
+                                        price1={val.price1}
+                                    />
+                                })
+                            }
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
