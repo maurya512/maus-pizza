06674b55eb208fe3f399531d05dff86b458b5721
maurya512 <patelmaurya0512@gmail.com>
2021-05-31 22:26:16 -0400

    Data component

---
+++ src/components/ChallengeData.js
@@ -0,0 +1,17 @@
+// import images to use for the challenges menu
+import pizza1 from '../images/pizza1.png'
+
+// array of objects 
+const ChallengeData = 
+[
+    {
+        imgsrc: pizza1,
+        title: "Italian Pizza Tossing Event",
+        description: "A fun event for novices and enthusiasts to get a lesson from the best pizza tosser in the house and put their newly accquired skills to a test. What's the prize you say?!",
+        prize: "Your meal on that night is on the house.",
+        time: "The event will be held every Tuesday evening from 6:00 PM to 8:00 PM"
+    }
+]
+
+// export the data
+export default ChallengeData
