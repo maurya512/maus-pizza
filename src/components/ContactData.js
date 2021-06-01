ca0e774cb37f99cf08ce8e597feccff15dddb6a9
maurya512 <patelmaurya0512@gmail.com>
2021-05-31 22:25:06 -0400

    Contact Data

---
+++ src/components/ContactData.js
@@ -0,0 +1,18 @@
+// the json file where the contact data will be stored
+// import the files and images needed for the card
+import pizza1 from '../images/pizza1.png'
+
+// array of objects
+const ContactData =
+    [
+        {
+            imgsrc: pizza1,
+            title: "Contact Us",
+            address: "745 Roosevelt Ln",
+            address1: "New York, NY-10007",
+            phone: "347-258-9634"
+        }
+    ]
+
+// export the data
+export default ContactData
