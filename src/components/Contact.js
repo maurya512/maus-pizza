344bf702d6744771ac1323d34b5b86c839d1b20e
maurya512 <patelmaurya0512@gmail.com>
2021-05-31 22:24:09 -0400

    imported the components

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,37 @@
 import React from 'react'
 
+// import files needed
+import ContactData from './ContactData'
+import ContactCard from './ContactCard'
+
+// functional component
 function Contact() {
     return (
-        <h1>
-           This is the contacts page 
-        </h1>
+        <>
+            <div className="my-5">
+                <h1 className="text-center"><strong>Contact Us</strong></h1>
+            </div>
+            <div className="container-fluid mb-5">
+                <div className="row">
+                    <div className="col-10 mx-auto">
+                        <div className="row gy-4">
+                            {/* mapping through the array of objects in the contactData */}
+                            {
+                                ContactData.map((val, ind) => {
+                                    return <ContactCard key={ind}
+                                        imgsrc={val.imgsrc}
+                                        title={val.title}
+                                        address={val.address}
+                                        address1={val.address1}
+                                        phone={val.phone}
+                                    />
+                                })
+                            }
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
