c122a9872b0ee959894e91401e619d6dbdc0282f
maurya512 <patelmaurya0512@gmail.com>
2021-05-31 22:23:41 -0400

    imported the components

--- src/components/Challenges.js
+++ src/components/Challenges.js
@@ -1,10 +1,36 @@
 import React from 'react'
 
+// import the files needed 
+import ChallengeData from './ChallengeData'
+import ChallengeCard from './ChallengeCard'
+
 function Challenges() {
     return (
-        <h1>
-           This the challenges page 
-        </h1>
+        <>
+            <div className="my-5">
+                <h1 className="text-center"><strong>Challenges</strong></h1>
+            </div>
+            <div className="container-fluid mb-5">
+                <div className="row">
+                    <div className="col-10 mx-auto">
+                        <div className="row gy-4">
+                            {/* mapping through the challenges data */}
+                            {
+                                ChallengeData.map((val, ind) => {
+                                    return <ChallengeCard key={ind}
+                                        imgsrc={val.imgsrc}
+                                        title={val.title}
+                                        description={val.description}
+                                        prize={val.prize}
+                                        time={val.time}
+                                    />
+                                })
+                            }
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
